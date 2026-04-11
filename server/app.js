38f742b0ec8c2d8bc1ac8299ac26a44bf8cc1e28
HectorGonzalez-dev <hector.gonzalez.dev@gmail.com>
2026-04-12 01:39:54 +0200

    agregar peliculas acabado

--- server/app.js
+++ server/app.js
@@ -361,6 +361,64 @@ app.post('/esborrarPeli', async (req, res) => {
   }
 })
 
+app.get('/movieAdd', async (req, res) => {
+  try {
+
+    const langRows = await db.query(`SELECT language_id, name FROM language ORDER BY name`);
+
+    const langJson = db.table_to_json(langRows, {
+      language_id: 'number',
+      name: 'string'
+    })
+
+    // Llegir l'arxiu .json amb dades comunes per a totes les pàgines
+    const commonData = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
+    );
+
+    // Construir l'objecte de dades per a la plantilla
+    const data = {
+      language: langJson,
+      common: commonData
+    };
+
+    // Renderitzar la plantilla amb les dades
+    res.render('movieAdd', {
+        ...data,
+        currentPage: 'movieAdd'
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error consultant la base de dades');
+  }
+});
+
+app.post('/afegirPeli', async (req, res) => {
+  try {
+
+    const table = req.body.table
+
+    if (table == "film") {
+
+      const title = req.body.title
+      const description = req.body.description
+      const release_year = parseInt(req.body.release_year, 10)
+      const language_id = parseInt(req.body.language, 10)
+      const length = parseInt(req.body.length, 10)
+      const rating = req.body.rating
+
+      await db.query(`INSERT INTO film (title, description, release_year, language_id, length, rating) VALUES ("${title}", "${description}", ${release_year}, ${language_id}, ${length}, "${rating}")`)
+
+      // Redirect to list of courses
+      res.redirect('/movies')
+    }
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Error afegint el curs')
+  }
+})
+
 app.get('/customers', async (req, res) => {
   try {
     // Obtenir les dades de la base de dades
